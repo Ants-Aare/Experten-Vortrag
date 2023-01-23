23fe5fadec4363055a0f35a9b466c8bbf37f9f4c
antsaare-popcore <alamaa.ants@popcore.com>
2023-01-23 10:14:51 +0100

    Added Playermovement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using AAA.Editor.Runtime.Attributes;
+using UnityEngine;
+using NaughtyAttributes;
+
+namespace AAA.XYRalityTest
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private float movementSpeed;
+        [ShowNonSerializedField, ReadOnly] private Vector3 _inputMoveDirection = Vector3.zero;
+        [SerializeField, Self] private Rigidbody rBody;
+
+        void FixedUpdate()
+        {
+            ReceiveInputs();
+            ApplyMovement();
+        }
+
+        private void ReceiveInputs()
+        {
+            _inputMoveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        }
+
+        private void ApplyMovement()
+        {
+            rBody.velocity = _inputMoveDirection * movementSpeed;
+        }
+    }
+}
